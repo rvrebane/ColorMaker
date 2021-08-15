9e8391186617067cabd1692b6d80e3ce5ee18799
Rein Saetalu <reinukas1@gmail.com>
2021-08-15 22:47:07 +0300

    Initial commit

--- ColorMaker/ViewController.swift
+++ ColorMaker/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func changeColorComponent() {
+        
+        
+        // Make sure the program doesn't crash if the controls aren't connected
+        if self.redControl == nil {
+            return
+        }
+        
+        let r = CGFloat(redControl.value)
+        let g = CGFloat(greenControl.value)
+        let b = CGFloat(blueControl.value)
+                
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
 
 
